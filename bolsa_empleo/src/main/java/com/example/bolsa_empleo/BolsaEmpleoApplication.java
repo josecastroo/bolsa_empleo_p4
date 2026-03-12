cca6126f489b647062446631398c76dd8d908c97
josecastroo <castrooj02@gmail.com>
2026-03-12 13:05:24 -0600

    project init

---
+++ bolsa_empleo/src/main/java/com/example/bolsa_empleo/BolsaEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolsa_empleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaEmpleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolsaEmpleoApplication.class, args);
+    }
+
+}
---
+++ bolsa_empleo/src/test/java/com/example/bolsa_empleo/BolsaEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolsa_empleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaEmpleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
