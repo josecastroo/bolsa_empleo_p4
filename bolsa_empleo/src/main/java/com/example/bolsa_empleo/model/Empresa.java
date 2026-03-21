0501faeb8ed5fa17c9a7209c603dc2a3fbc056a2
jose <castrooj02@gmail.com>
2026-03-20 19:56:45 -0600

    feat: create Empresa entity and db table
    - Defined Empresa entity
    - Added empresa table in bolsa_empleo database

---
+++ bolsa_empleo/src/main/java/com/example/bolsa_empleo/model/Empresa.java
@@ -0,0 +1,84 @@
+package com.example.bolsa_empleo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String location;
+    private String email;
+    private String phone;
+
+    @Column(length = 1000)
+    private String description;
+
+    private boolean approved;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
