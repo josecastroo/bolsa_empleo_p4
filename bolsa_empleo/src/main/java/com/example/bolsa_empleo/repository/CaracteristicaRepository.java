d1d276dcb69daa484fd3ca09fa398f9b1548be17
jose <castrooj02@gmail.com>
2026-03-20 19:22:59 -0600

    CaracteristicaRepository created

---
+++ bolsa_empleo/src/main/java/com/example/bolsa_empleo/repository/CaracteristicaRepository.java
@@ -0,0 +1,7 @@
+package com.example.bolsa_empleo.repository;
+
+import com.example.bolsa_empleo.model.Caracteristica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Long> {
+}
